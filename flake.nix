8b1cf15b88f94c129234b7d91d128fdd2fe45ff5
maeve <maeve@oa.ke>
2026-01-17 15:36:31 +0000

    flake: update

--- flake.nix
+++ flake.nix
@@ -1,54 +1,30 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    pebble = {
-      url = "github:anna-oake/pebble.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
+    pebble.url = "github:anna-oake/pebble.nix";
   };
 
   outputs =
     {
       self,
       pebble,
-      nixpkgs,
       flake-utils,
       ...
     }:
     let
       packageJson = builtins.fromJSON (builtins.readFile ./package.json);
-      inherit (packageJson) name;
-      emulatorTarget = "emery";
+      inherit (pebble) mkAppInstallPbw buildPebbleApp pebbleEnv;
     in
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        apps.default = {
-          type = "app";
-          program = toString (
-            pkgs.writeShellScript "install-${name}" ''
-              set -euo pipefail
-              exec ${pebble.packages.${system}.pebble-tool}/bin/pebble install "${
-                self.packages.${system}.default
-              }/${name}.pbw" --emulator ${emulatorTarget} --logs "$@"
-            ''
-          );
-        };
+    flake-utils.lib.eachDefaultSystem (system: {
+      apps.default = mkAppInstallPbw.${system} {
+        pbwPackage = self.packages.${system}.default;
+      };
 
-        packages.default = pebble.buildPebbleApp.${system} {
-          inherit (packageJson) name version;
-          src = ./.;
-        };
+      packages.default = buildPebbleApp.${system} {
+        inherit (packageJson) name version;
+        src = ./.;
+      };
 
-        devShell = pebble.pebbleEnv.${system} {
-          inherit emulatorTarget;
-        };
-      }
-    );
+      devShell = pebbleEnv.${system} { };
+    });
 }
